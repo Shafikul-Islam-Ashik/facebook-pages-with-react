c6b6c853629fb8edc8fccdd966e9ff4fa9e3af58
Shafikul Islam Molla Ashik <ishafikul720@gmail.com>
2023-12-03 11:45:19 +0600

    init setup done

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,6 +1,10 @@
+import Meta from "../../components/meta/Meta";
+import "./Auth.scss";
+
 const Auth = () => {
   return (
     <>
+      <Meta title="Facebook - log in or sign up" />
       <h1>Auth Page</h1>
     </>
   );
--- src/router/publicRouter.jsx
+++ src/router/publicRouter.jsx
@@ -1,10 +1,15 @@
 import Auth from "../pages/auth/Auth";
+import Home from "../pages/home/Home";
 
 // create private router
 const publicRouter = [
   {
     path: "/",
     element: <Auth />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
   },
 ];
 
